5f92ffe2444fef2bd050abd4eb5a2e136c531e6e
Tohmine Nao <n.tohmine.s3@gmail.com>
2022-08-19 00:02:14 +0900

    Revert "Modify: Breweryモデルのカラムを修正#35"
    This reverts commit 10934e8aba67d863321016407c4aa12205bdd870.

--- db/migrate/20220818142924_remove_image_from_breweries.rb
+++
@@ -1,9 +0,0 @@
-class RemoveImageFromBreweries < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :breweries, :image, :string
-  end
-
-  def down
-    add_column :breweries, :image, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_18_142924) do
+ActiveRecord::Schema.define(version: 2022_08_01_131245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_08_18_142924) do
     t.string "prefecture"
     t.integer "liquor_type"
     t.string "website_url"
+    t.string "image"
     t.string "video_id"
     t.float "latitude"
     t.float "longitude"
