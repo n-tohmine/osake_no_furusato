f287fc3f1d12e8b5ade05829f2a353199a4f7a3a
Tohmine Nao <n.tohmine.s3@gmail.com>
2022-08-22 23:50:59 +0900

    Modify: breweryモデルのカラム修正#35

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_01_131245) do
+ActiveRecord::Schema.define(version: 2022_08_22_134140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,13 @@ ActiveRecord::Schema.define(version: 2022_08_01_131245) do
     t.string "prefecture"
     t.integer "liquor_type"
     t.string "website_url"
-    t.string "image"
     t.string "video_id"
     t.float "latitude"
     t.float "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "place_id"
+    t.string "image"
   end
 
 end
