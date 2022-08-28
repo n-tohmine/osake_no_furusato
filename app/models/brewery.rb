59d1428e069034ddc39c4de20f7982004fdf3915
Tohmine Nao <n.tohmine.s3@gmail.com>
2022-08-28 10:13:40 +0900

    Revert "Add: brewery初期データ投入#36"
    This reverts commit 0875d7a31b8293f0a83c02ac8bb0f3c6831a61f3.

--- app/models/brewery.rb
+++
@@ -1,8 +0,0 @@
-class Brewery < ApplicationRecord
-  has_many :keeps, dependent: :destroy
-  has_many :likes, dependent: :destroy
-  has_many :reviews, dependent: :destroy
-
-  enum liquor_type:{beer: 0, wine: 1, sake: 2, whiskey: 3}
-  
-end
