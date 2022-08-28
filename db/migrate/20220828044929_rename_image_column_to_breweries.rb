62612768a14382c1e31ba5d85ec51b809f9f2cc2
Tohmine Nao <n.tohmine.s3@gmail.com>
2022-08-28 15:33:24 +0900

    Revert "Modify: brewery初期データの修正と再インポート#42"
    This reverts commit 0ec08e63976fb2bc12ad3839309caced93c2f69c.

--- db/migrate/20220828044929_rename_image_column_to_breweries.rb
+++
@@ -1,5 +0,0 @@
-class RenameImageColumnToBreweries < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :breweries, :image, :photo_reference
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_28_044929) do
+ActiveRecord::Schema.define(version: 2022_08_22_134140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2022_08_28_044929) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "place_id"
-    t.string "photo_reference"
+    t.string "image"
   end
 
 end
