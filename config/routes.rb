bf348ab3abdba282f6aace48f3e55fa06d5d3c53
Tohmine Nao <n.tohmine.s3@gmail.com>
2022-10-13 23:55:25 +0900

    Add: 付近のホテル一覧画面を作成#78

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root  to: 'home#top'
-  resources :breweries, only: %i[index show]
+  resources :breweries, only: %i[index show] do
+    get 'nearby_hotels',on: :member
+  end
   get 'search_from_japanmap', to: 'search_from_japanmap#index'
   get 'search_from_liquor_type', to: 'search_from_liquor_type#index'
   
