0144d31a4b9741d14119cdd3a7bd5578b726d12f
Tohmine Nao <n.tohmine.s3@gmail.com>
2023-04-07 17:20:01 +0900

    Edit application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require ("jquery.raty.js")
 
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
